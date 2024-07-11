2d77dbd98b0274f51c93159a2c6e1abf99eff7f9
Ashar Khan <asharullah.khan2468@gmail.com>
2024-07-11 22:28:35 +0500

    Weather App completed

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
+const input = document.querySelector("#default-search");
+const div = document.querySelector("#div");
+let submitBtn = document.querySelector("#search-btn").addEventListener('click' , (event)=>{
 
+  event.preventDefault();
 
-
-
-
-
-
-
+  
 axios(
-  `http://api.weatherapi.com/v1/current.json?key=068cb42292994c9480f121502241107&q=karachi&aqi=no`
+  `http://api.weatherapi.com/v1/current.json?key=068cb42292994c9480f121502241107&q=${input.value}&aqi=no`
 )
   .then((resp) => {
-    console.log(resp.data);
+    div.innerHTML = ` <div class="weather-card " id="weather-card">
+        <h3 id="city-name">${resp.data.location.name}, ${resp.data.location.region} , ${resp.data.location.country}</h3>
+        <div class="weather-icon" id="weather-icon"><img class="center size" src="${resp.data.current.condition.icon}" alt="icon"></div>
+        <div class="temperature" id="temperature">${resp.data.current.temp_c}°C</div>
+        <div class="description" id="description">Wind Speed: ${resp.data.current.wind_kph} Kph</div>
+    </div>`;
+
+    
+
+
   })
 
   .catch((err) => {
     console.log(err);
   });
+
+})
+
+
+
+
+
